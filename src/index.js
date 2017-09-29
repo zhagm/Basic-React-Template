4047450ab322e0ce56467665b3a29509becc8a92
zhagm <zhagdev@gmail.com>
2017-09-29 13:29:30 +0700

    init, functional

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+require('./index.css');
+
+ReactDOM.render(
+  <h1>Hello World</h1>,
+  document.getElementById('root')
+);
